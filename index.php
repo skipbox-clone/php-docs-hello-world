9c962be6275ce894b2daeb4ba92f759289f90109
skipbox-clone <81555305+skipbox-clone@users.noreply.github.com>
2022-02-13 17:03:39 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
 echo "Hello World!";
+echo "KBTC Internatioal School Of IT Team Member List";
+echo "(1) Toe Lwin Myint";
+echo "(2) Hnin Zin Phyu Thant";
+echo "(3) Wai Yan Aung";
+echo "(4) Aung Thuya Htet";
